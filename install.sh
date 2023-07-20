19f7fed70200a61d2edd0ca767272df7dfde64c0
Rico98 <asd.cqd98@gmail.com>
2023-07-20 11:35:16 -0500

    Added installation bash script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "venv" ]; then
+  # Create virtual environment
+  python -m venv env
+fi
+
+# Activate virtual environment
+source env/Scripts/activate
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install pytorch
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+
+# Install python dependencies from requirements.txt
+pip install -r requirements.txt
